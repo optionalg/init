7b42c3c861800cee02893ebd90d48f0b43c9c75e
David <davidcondrey@me.com>
2014-12-06 21:11:15 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,34 @@
+#!/bin/bash
+#
+# move .bash_profile and .bashrc into place
+mv *.bash* ~/
+
+# xcode command-line tools
+xcode-select --install
+
 # homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
+
+# wget
+curl http://ftp.gnu.org/gnu/wget/wget-1.15.tar.xz > wget-1.15.tar.xz &&
+tar -xzf wget-1.15.tar.xz && cd wget-1.15 && ./configure --with-ssl=openssl && make -j8 && sudo make install &&
+cd .. && rm -rf wget*
 
 # setup git
 git config --global user.name "davidcondrey"
 git config --global user.email "davidcondrey@me.com"
 git config --global color.ui auto
 
+
 # set textmate as default for git
 git config --global core.editor "mate -w"
-
-
-# textmate
+## textmate bundles
 BUNDLES_PATH=`pwd`
-
 mkdir -p ~/Library/Application\ Support/TextMate/Bundles
 rm -rf ~/Library/Application\ Support/TextMate/Bundles
-
 mkdir -p Bundles
 ln -sf "$BUNDLES_PATH/Bundles" ~/Library/Application\ Support/TextMate/Bundles
-
 get_bundle() {
   (
   if [ -d "$3" ]; then
@@ -28,15 +37,9 @@ get_bundle() {
     git pull --rebase
   else
     git clone "git://github.com/$1/$2.git" "$3"
-    # if [[ "$2" = "git-tmbundle" ]]; then
-    #   gem install git
-    # elif [[ "$2" = "SCSS.tmbundle" ]]; then
-    #   gem install nokogiri
-    # fi
   fi
   )
 }
-
 get_bundle drnic github-tmbundle "Bundles/github.tmbundle"
 get_bundle fgnass jshint.tmbundle "Bundles/jshint.tmbundle"
 get_bundle grorg/svg-tmbundle.git "Bundles/svg.tmbundle
@@ -50,9 +53,6 @@ get_bundle textmate markdown.tmbundle "Bundles/markdown.tmbundle"
 get_bundle textmate mediawiki.tmbundle "Bundles/mediawiki.tmbundle"
 get_bundle textmate textile.tmbundle "Bundles/textile.tmbundle"
 get_bundle vigo textmate-twitterbootstrap.tmbundle "Bundles/twitterbootstrap.tmbundle"
-
 sudo ln -s /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc /usr/local/bin
-
 osascript -e 'tell app "TextMate" to reload bundles'
-
 echo "Textmate setup complete"
