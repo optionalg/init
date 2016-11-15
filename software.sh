dddfbb46d6b8c5c1b6dabf080f8efd8052a0c521
dcondrey <davidcondrey@me.com>
2016-11-14 18:33:15 -0800

    software update

--- software.sh
+++ software.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Fix permissions on /usr/local/
-sudo chflags norestricted /usr/local && sudo chown $(whoami):admin /usr/local && sudo chown -R $(whoami):admin /usr/local
+# Remove junk applications
+rm -rf /Applications/GarageBand.app
+rm -rf /Applications/iMovie.app
+rm -rf /Applications/iTunes.app
+rm -rf "/Library/Application Support/GarageBand"
+rm -rf "/Library/Application Support/Logic"
+rm -rf "/Library/Audio/Apple Loops"
 
-#
 # Check if Homebrew is installed
-#
 which -s brew
 if [[ $? != 0 ]] ; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -13,176 +16,179 @@ else
     brew update
 fi
 
+# Subscribe to taps
+brew tap homebrew/services
+brew tap homebrew/completions
 brew tap homebrew/dupes
 brew tap homebrew/versions
+brew tap homebrew/fuse
+brew tap homebrew/gui
 
-#
+brew tap caskroom/fonts
+brew tap caskroom/versions
+
+brew tap macvim-dev/macvim
+
 # Check if Git is installed
-#
 which -s git || brew install git
 
+sudo /usr/bin/gem update --system
+
 apps=(
-  adium
-  appcleaner
-  betterzipql
-  cyberduck
-  dropbox
-  firefox
-  github
-  google-chrome
-  imageoptim
-  iterm2
-  java
-  nvalt
-  qlcolorcode
-  qlmarkdown
-  qlprettypatch
-  qlstephen
-  quicklook-csv
-  quicklook-json
-  rescuetime
-  sequel-pro
-  shiori
-  skype
-  slack
-  sophos-anti-virus-home-edition
-  sublime-text3
-  transmission
-  transmit
-  vagrant
-  virtualbox
-  vlc
-  webpquicklook
+    '1password'
+    'adium'
+    'alfred'
+    'appcleaner'
+    'avira-antivirus'
+    'avira-vpn'
+    'betterzipql'
+    'docker-toolbox'
+    'dropbox'
+    'electron'
+    'epubquicklook'
+    'firefox-beta'
+    'flux'
+    'font-anonymous-pro'
+    'font-dejavu-sans-mono-for-powerline'
+    'font-droid-sans'
+    'font-droid-sans-mono'
+    'font-inconsolata'
+    'font-input'
+    'font-liberation-sans'
+    'font-nixie-one'
+    'font-pt-mono'
+    'font-roboto'
+    'font-source-code-pro'
+    'font-source-sans-pro'
+    'google-chrome-canary'
+    'google-drive'
+    'gpgtools'
+    'imageoptim'
+    'iterm2-beta'
+    'java'
+    'jumpcut'
+    'monolingual'
+    'nvalt'
+    'odrive'
+    'osxfuse-beta'
+    'qlcolorcode'
+    'qlimagesize'
+    'qlmarkdown'
+    'qlprettypatch'
+    'qlstephen'
+    'quicklook-csv'
+    'quicklook-json'
+    'shortcat'
+    'slack-beta'
+    'sublime-text-dev'
+    'transmission-nightly'
+    'transmit'
+    'vagrant'
+    'virtualbox'
+    'vlc'
+    'webpquicklook'
 )
 
 brews=(
-  coreutils
-  binutils
-  diffutils
-  gawk
-  wget --enable-iri
-  bash
-  bash-completion2
-  fish
-  grep
-  ed --default-names
-  findutils --with-default-names
-  gnu-getopt --with-default-names
-  gnu-indent --with-default-names
-  gnu-tar --with-default-names
-  gnu-sed --with-default-names
-  gnutls --with-default-names
-  python --with-brewed-openssl
-  openssh --with-brewed-openssl
-  vim --with-lu --override-system-vi
-  macvim --HEAD --with-cscope --with-lua --with-override-system-vim --with-luajit --with-python
-  ack
-  bfg
-  binwalk
-  boost
-  closure-compiler
-  colordiff
-  dex2jar
-  dns2tcp
-  foremost
-  freetype
-  gcc
-  gettext
-  gist
-  git-lfs
-  gmp
-  gpg
-  highlight
-  icu4c
-  imagemagick
-  imagesnap
-  isl
-  jasper
-  jpeg
-  knock
-  libdnet
-  libidn
-  libmpc
-  libogg
-  libpng
-  libtiff
-  libtool
-  lua
-  lynx
-  maven
-  mcrypt
-  mhash
-  moreutils
-  mpfr
-  mysql
-  narwhal
-  netpbm
-  nmap
-  openssl
-  p7zip
-  pcre
-  php56
-  php56-mcrypt
-  pigz
-  pkg-config
-  pngcheck
-  potrace
-  pv
-  pyside
-  qt
-  readline
-  rename
-  rhino
-  ringojs
-  screen
-  sfnt2woff
-  sfnt2woff-zopfli
-  shiboken
-  socat
-  speedtest_cli
-  sqlite
-  tcptrace
-  terminal-notifier
-  tmux
-  tree
-  ucspi-tcp
-  unixodbc
-  v8
-  vbindiff
-  webkit2png
-  webp
-  woff2
-  xz
-  zopfli
-  zsh
+    'aircrack-ng'
+    'autoconf'
+    'automake'
+    'bash'
+    'bash-completion2'
+    'binutils'
+    'binwalk'
+    'brew-gem'
+    'closure-compiler'
+    'colordiff'
+    'coreutils'
+    'dex2jar'
+    'diffutils'
+    'dns2tcp'
+    'doxygen'
+    'findutils --with-default-names'
+    'fish'
+    'foremost'
+    'freetype'
+    'gem-completion'
+    'gettext'
+    'gist'
+    'git'
+    'git-extras'
+    'git-utils'
+    'grep'
+    'htop'
+    'imagemagick --with-librsvg'
+    'imagesnap'
+    'lynx'
+    'maven'
+    'mcrypt'
+    'meld'
+    'mysql'
+    'nmap --HEAD --with-pygtk'
+    'openssh --with-brewed-openssl'
+    'openssl'
+    'pcre'
+    'php56'
+    'php56-mcrypt'
+    'pigz'
+    'pkg-config'
+    'pngcheck'
+    'potrace'
+    'pv'
+    'python --HEAD'
+    'python3 --HEAD'
+    'readline'
+    'sfnt2woff'
+    'sfnt2woff-zopfli'
+    'socat'
+    'speedtest_cli'
+    'sqlite --with-json1'
+    'tcptrace'
+    'terminal-notifier'
+    'tmux'
+    'tree'
+    'v8'
+    'vbindiff'
+    'webkit2png'
+    'webp --HEAD --with-libtiff'
+    'wget --enable-iri'
+    'woff2'
+    'xz'
+    'zopfli'
 )
 
-npm=(
-  bower
-  csslint
-  eslint
-  express
-  glob
-  grunt
-  grunt-cli
-  handlebars
-  jscs
-  jshint
-  lodash
-  matchdep
-  nodemon
-  yo
+gems=(
+    'bundler'
+    'compass'
+    'jsdoc'
+    'nokogiri'
+    'sass'
 )
 
-# Remove packages from brews list that are already installed
-for (( i=0; i < ${#brews[@]}; i++ )); do
-    brew ls | grep ${brews[i]}
-    if [[ $? != 0 ]] ; then
-        brews=( "${brews[@]:0:$i}" "${brews[@]:$((i + 1))}" )
-    fi
-done
+modules=(
+    'bower'
+    'csslint'
+    'electron'
+    'eslint'
+    'express'
+    'glob'
+    'grunt'
+    'grunt-cli'
+    'gulp-cli'
+    'handlebars'
+    'jscs'
+    'jshint'
+    'less'
+    'lodash'
+    'matchdep'
+    'nodemon'
+    'react'
+    'underscore'
+    'yo'
+)
 
-brew link curl --force
+brew install --HEAD macvim-dev/macvim/macvim --HEAD --with-lua --with-luajit --with-override-system-vim --with-python3
+
 sudo echo $(brew --prefix)/bin/bash >> /etc/shells && \
 chsh -s $(brew --prefix)/bin/bash
 
@@ -191,41 +197,55 @@ echo "$(brew --prefix)/bin/fish" | sudo tee -a /etc/shells
 chsh -s $(brew --prefix)/bin/fish
 mkdir -p ~/.config/fish
 
-# Install apps to /Applications rather than ~/Applications
-printf "\n%s\n" "installing ${brews[@]}.."
-brew install ${brews[@]}
-
-
-# Remove packages from apps list that are already installed
-for (( i=0; i < ${#apps[@]}; i++ )); do
-    brew cask ls | grep ${apps[i]}
-    if [[ $? != 0 ]] ; then
-        apps=( "${apps[@]:0:$i}" "${apps[@]:$((i + 1))}" )
-    fi
-done
-
-# Install apps to /Applications rather than ~/Applications
-printf "\n%s\n" "installing ${apps[@]}.."
-brew cask install --appdir="/Applications" ${apps[@]}
-
-brew ls
-brew cask ls
-
-brew upgrade
-brew cleanup
-
-
-$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
-
-# install node
+# Install Node
 curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
 
 # Ensure NODE_PATH is set
 grep NODE_PATH ~/.bash_profile > /dev/null || cat "export NODE_PATH=/usr/local/lib/node_modules" >> ~/.bash_profile && . ~/.bash_profile
 grep MANPATH ~/.bash_profile > /dev/null || cat "export $MANPATH=$(brew --prefix coreutils)/libexec/gnuman:$MANPATH" >> ~/.bash_profile && . ~/.bash_profile
 
-echo "installing npm packages.."
-for n in "${npm[@]}"; do:
-  echo "..${n}"
-  npm install -g ${n}
+# Install apps to /Applications rather than ~/Applications
+echo "installing Homebrew packages.."
+for brew in "${brews[@]}"; do:
+  echo "..${brew}"
+  brew install ${brew}
 done
+
+echo "installing Ruby gems.."
+for gem in "${gems[@]}"; do:
+  echo "..${gem}"
+  brew gem install ${gem}
+done
+
+echo "installing Applications.."
+for app in "${apps[@]}"; do:
+  echo "..${apps}"
+  brew cask install --appdir="/Applications" ${apps}
+done
+
+echo "installing node modules.."
+for mod in "${modules[@]}"; do:
+  echo "..${mod}"
+  npm install -g ${mod}
+done
+
+pip install --upgrade pip
+pip3 install --upgrade pip setuptools wheel
+
+brew link curl --force
+brew linkapps macvim
+brew linkapps python
+brew linkapps python3
+
+
+
+# Fix permissions on /usr/local/
+sudo chflags norestricted /usr/local && sudo chown $(whoami):admin /usr/local && sudo chown -R $(whoami):admin /usr/local
+
+brew cask update
+brew cask cleanup
+brew upgrade
+brew cleanup
+brew doctor
+
+
